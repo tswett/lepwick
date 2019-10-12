962606f094691380711cd9758541d9f269331bec
Tanner Swett <tswett@stilesmachinery.com>
2019-10-12 15:57:01 -0400

    Create template for main view

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from .models import UserHolding
 
 def index(request):
-    return HttpResponse('Hello, world. You are a colonist on Mars.')
+    all_holdings = UserHolding.objects.all()
+    template = loader.get_template('mainapp/index.html')
+    context = { 'holdings': all_holdings }
+
+    return HttpResponse(template.render(context, request))
