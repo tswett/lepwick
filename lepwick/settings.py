38fd369eb73b9914d67f62323569f9a1f09473a3
Tanner Swett <tswett@stilesmachinery.com>
2019-10-12 15:05:47 -0400

    Add Commodity model

--- lepwick/settings.py
+++ lepwick/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mainapp.apps.MainappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Commodity(models.Model):
+    name = models.CharField(max_length=50)
+    minor_unit_plural = models.CharField('minor unit (plural)', max_length=50)
+
+    def __str__(self):
+        return self.name
